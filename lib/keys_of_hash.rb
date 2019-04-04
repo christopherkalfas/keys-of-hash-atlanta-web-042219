16719b04127f6cd6e6d7aa997a3e204715e6217a
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-04 21:26:09 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,9 @@
-require'pry'
+
 class Hash
   def keys_of(*arguments)
     self.each do |key, value|
       if arguments.include?(value)
-    puts "#{arguments}"
-    binding.pry
+  
   end
    
 end
