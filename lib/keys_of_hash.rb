7f6ecd2e29044c3d6e6db1619f55f08af5c2a71c
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-04 21:35:47 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,11 @@
 
 class Hash
   def keys_of(*arguments)
-   
+   new_array= []
     self.each do |key, value|
       if arguments.include?(value)
-        key.flatten
+        new_array << key
+        new_array.flatten
       end 
     end 
   end
